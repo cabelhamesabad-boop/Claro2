5d400a8f29ce4cc59df9ee2e436a8fa449ebf1a3
cabelhamesabad-boop <cabelhamesabad@gmail.com>
2026-09-11 11:51:12 -0500

    apk3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
                 </p>
               </div>
               <div className="space-x-4">
-                <a href="https://u3ea0s9znkxuip15.public.blob.vercel-storage.com/CLARO_5G.apk">
+                <a href="https://u3ea0s9znkxuip15.public.blob.vercel-storage.com/CLARO_5G.apk>
                   <Button className="bg-white text-red-600 hover:bg-gray-100">
                     Descargar Ahora
                   </Button>
